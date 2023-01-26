8fd6fba44e09bec068a7c89fe46f252d30c40d8a
pachperdev <pachperdev@gmail.com>
2023-01-25 23:02:04 -0500

    Statusbar y títulos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,54 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: const [
           Background(),
+          _HomeBody(),
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: const [PageTitle()],
+      ),
+    );
+  }
+}
+
+class PageTitle extends StatelessWidget {
+  const PageTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 60, horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Clasify transaccion',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Clasify this transaccion into a particular Category',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 19,
+            ),
+          ),
         ],
       ),
     );
